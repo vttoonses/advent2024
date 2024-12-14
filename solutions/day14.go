049832646057fb82f50afecd88dcdc390eba4a6e
Eugene Thompson <gene@thompson93.us>
2024-12-14 15:10:02 -0500

    use math instead of loops

--- solutions/day14.go
+++ solutions/day14.go
@@ -34,14 +34,14 @@ func (data *Day14) ProcessDataSet() error {
 		data.Seconds = 0
 		for ; !data.isTree(); data.Seconds++ {
 			for lcv := 0; lcv < len(data.robots); lcv++ {
-				data.robots[lcv].move(data.Width, data.Height)
+				data.robots[lcv].position.x = pmod((data.robots[lcv].position.x + data.robots[lcv].velocity.x), data.Width)
+				data.robots[lcv].position.y = pmod((data.robots[lcv].position.y + data.robots[lcv].velocity.y), data.Height)
 			}
 		}
 	} else {
-		for ; data.Seconds > 0; data.Seconds-- {
-			for lcv := 0; lcv < len(data.robots); lcv++ {
-				data.robots[lcv].move(data.Width, data.Height)
-			}
+		for lcv := 0; lcv < len(data.robots); lcv++ {
+			data.robots[lcv].position.x = pmod((data.robots[lcv].position.x + (data.robots[lcv].velocity.x * data.Seconds)), data.Width)
+			data.robots[lcv].position.y = pmod((data.robots[lcv].position.y + (data.robots[lcv].velocity.y * data.Seconds)), data.Height)
 		}
 	}
 	return nil
@@ -49,18 +49,6 @@ func (data *Day14) ProcessDataSet() error {
 
 func (data *Day14) Solve() (int, error) {
 	if data.Tree {
-		for y := 0; y < data.Height; y++ {
-			for x := 0; x < data.Width; x++ {
-				count := 0
-				for _, r := range data.robots {
-					if r.position.x == x && r.position.y == y {
-						count += 1
-					}
-				}
-				fmt.Print(count)
-			}
-			fmt.Println()
-		}
 		return data.Seconds, nil
 	}
 
@@ -82,24 +70,8 @@ func (data *Day14) Solve() (int, error) {
 			q4++
 		}
 	}
-	return q1 * q2 * q3 * q4, nil
-}
 
-func (r *robot) move(width, height int) {
-	r.position.x += r.velocity.x
-	r.position.y += r.velocity.y
-	if r.position.x >= width {
-		r.position.x -= width
-	}
-	if r.position.x < 0 {
-		r.position.x = width + r.position.x
-	}
-	if r.position.y >= height {
-		r.position.y -= height
-	}
-	if r.position.y < 0 {
-		r.position.y = height + r.position.y
-	}
+	return q1 * q2 * q3 * q4, nil
 }
 
 func (data *Day14) isTree() bool {
@@ -107,12 +79,23 @@ func (data *Day14) isTree() bool {
 	count := 0
 
 	for lcv, r1 := range data.robots {
-		for _, r2 := range data.robots[lcv + 1:] {
-			if abs(r1.position.x - r2.position.x) <= 1 && abs(r1.position.y - r2.position.y) <= 1 {
+		for _, r2 := range data.robots[lcv+1:] {
+			if abs(r1.position.x-r2.position.x) <= 1 && abs(r1.position.y-r2.position.y) <= 1 {
 				count += 1
 			}
 		}
 	}
 
 	return count >= majority
+}
+
+func pmod(x, d int) int {
+	x = x % d
+	if x >= 0 {
+		return x
+	}
+	if d < 0 {
+		return x - d
+	}
+	return x + d
 }
